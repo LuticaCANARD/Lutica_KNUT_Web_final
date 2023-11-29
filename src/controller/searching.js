8b33420d0d4edb54f23ae1a658e846f283c3bf2a
Lutica_CANARD <presan100@gmail.com>
2023-11-30 02:28:28 +0900

    ✅ add : 하이퍼 링크 구현

--- src/controller/searching.js
+++ src/controller/searching.js
@@ -7,7 +7,7 @@ import {searchingPeople,searchingRandomPeople} from '../model/searching.js';
 * @param {NextFunction} next 
 */
 export const searchingPeopleRouter = async(req,res,next) =>{
-	const data = await searchingPeople(req.query["keyword"]);
+	const data = await searchingPeople(req.query["keyword"],req.session?.passport?.user);
 	const randomData = await searchingRandomPeople(req.session?.passport?.user,10);
 	res.render('search.ejs',{data,randomData});
 };
--- src/model/searching.js
+++ src/model/searching.js
@@ -1,8 +1,8 @@
 import { db,db_common } from '../utils/mysqldb.js';
 
 
-export const searchingPeople = async(usernick) =>{
-	const ret =  await db.query('SELECT mainDesc,nickname,mainProfilePicture,mainTitlePicture,id FROM user WHERE nickname LIKE ? ',['%'+usernick+'%']);
+export const searchingPeople = async(usernick,me) =>{
+	const ret =  await db.query('SELECT mainDesc,nickname,mainProfilePicture,mainTitlePicture,id FROM user WHERE nickname LIKE ? AND id!= ? AND id NOT IN (SELECT targetId FROM Social WHERE followerId=?)  ',['%'+usernick+'%',me,me]);
 	return ret[0];
 };
 
