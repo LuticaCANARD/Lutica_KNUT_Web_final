6e1a0264afdcb643ee7670faa21cf7cef907c22d
Lutica_CANARD <presan100@gmail.com>
2023-11-30 21:50:15 +0900

    가져오는 기능들 추가

--- src/controller/Post/posts.js
+++ src/controller/Post/posts.js
@@ -1,8 +1,8 @@
 import {uploadPostModel,loadPostModel} from '../../model/Post/posts.js';
 import fs from 'fs';
 import { cleanText } from '../../utils/text.js';
-import  DOMPurify  from 'dompurify';
-import { Marked } from 'marked';
+import DOMPurify from 'isomorphic-dompurify';
+import { parse } from 'marked';
 /**
  * 
  * @param {Request} req 
@@ -12,7 +12,7 @@ import { Marked } from 'marked';
 export const loadPost = async (req,res,next) =>{
 	const id = req.params.postId;
 	const post = await loadPostModel(id);
-	res.render('postMain.ejs',post);
+	res.render('postMain.ejs',{post});
 };
 
 /**
@@ -25,7 +25,8 @@ export const uploadPost = async (req,res,next) =>{
 	const text_ = fs.readFileSync(`upload/${req.body["filename"]}`);
 	const text = text_.toString('utf-8');
 	const obj = JSON.parse(req.body["object"]??'{}');
-	const parsedText = DOMPurify.sanitize(Marked.parse(text));
+	const _text = parse(text);
+	const parsedText = DOMPurify.sanitize(_text);
 	const userId = req.session?.passport?.user;
 	fs.rmSync(`upload/${req.body["filename"]}`,{force: true});
 	const ret = await uploadPostModel(userId,req.body["title"]??'제목 없는 글',parsedText,obj);
--- src/model/Post/posts.js
+++ src/model/Post/posts.js
@@ -23,6 +23,11 @@ export const uploadPostModel = async (userid,title,text,etc,fileid) =>{
 
 export const loadPostModel = async (id) =>{
 	const ret = await db.query('SELECT file,`desc`,location,A.createdAt,title,B.nickname FROM Post AS A INNER JOIN user AS B ON A.userId=B.id WHERE A.id=?',[id]);
+	let fileinfo = {};
+	if(ret[0][0].file!=null) {
+		fileinfo = await db.query("SELECT path FROM Picture AS A INNER JOIN PostPicture AS B ON A.id=B.pictureId WHERE B.postId=?",[id]);
+		fileinfo = fileinfo[0];
+	}
 	const mongoid = new ObjectId(ret[0][0].desc);
 	await mongodb.connect();
 	const obj = await mongodb.db('parareal').collection('post').findOne({_id:mongoid});
@@ -30,6 +35,7 @@ export const loadPostModel = async (id) =>{
 
 	return {
 		meta:ret[0][0],
-		context : obj
+		context : obj,
+		fileinfo 
 	};
 };
