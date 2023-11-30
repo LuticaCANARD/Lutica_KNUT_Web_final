b215b92deb04a938ee1b96f03118a3da116bde34
Lutica_CANARD <presan100@gmail.com>
2023-12-01 00:43:37 +0900

    ㅁㅁ

--- src/controller/Content/userFile.js
+++ src/controller/Content/userFile.js
@@ -52,14 +52,14 @@ const processAfterUploadProfile = async(req,res,next) =>{
 const downloadFile = (req,res,next) =>{
 	const filename = req.params.filename+req.params[0];
 	let isFileExist;
-	isFileExist = fs.existsSync(`upload/${filename}`);// 파일 존재여부 확인
+	isFileExist = fs.existsSync(`./upload/${filename}`);// 파일 존재여부 확인
 	// 파일이 존재하지 않는다면 에러 처리
 	if (!isFileExist) {
 		res.status(500).send(false);
 	}
 	try {
 		// download()를 사용해서 파일을 프론트쪽으로 보내준다.
-		res.download(`upload/${filename}`);
+		res.download(`./upload/${filename}`);
 	} catch (err) {
 		res.status(500).send(true);
 	}
